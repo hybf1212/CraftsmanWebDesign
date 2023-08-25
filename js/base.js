3c60134be5710a6f5fb9d572573f0e6b634e77b1
hybf1212 <hybf1212@naver.com>
2023-08-25 16:51:44 +0900

    B-1 형 ( 좌우 슬라이드 )

---
+++ js/base.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    setInterval(function(){
+        $(".slide").animate({marginLeft : -1200},400,function(){
+            $(this).find("div").eq(0).appendTo($(this))
+            $(this).css("margin-left",0)
+        })
+    },3000)
+
+    $(".notice-gallery li > span").click(function(){
+        $(this).parent().addClass("on").siblings().removeClass('on')        
+    })
+   
+    $(".notice li").eq(0).click(function(){
+        $("#popup").removeClass("d-none")
+    })
+    $("#popup button").click(function(){
+        $("#popup").addClass("d-none")
+    })
+})
